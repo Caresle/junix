ca4fb2cbb891684b3651129dd84cb4e7d7447cae
Caresle <carabesle@gmail.com>
2025-01-22 19:51:18 -0600

    feat: add ifconfig command

--- src/main/java/com/caresle/junix/App.java
+++ src/main/java/com/caresle/junix/App.java
@@ -3,16 +3,16 @@ package com.caresle.junix;
 public class App {
   public static void main(String[] args) {
     // Temp main method, just testing the implementation of the commands
-    Ls ls = new Ls();
-    System.out.println("=== LS BEGIN ===");
-    ls.run();
-    System.out.println("=== LS END ===");
-
-    System.out.println("=== MV BEGIN ===");
-    Mv mv = new Mv();
-    mv.run();
-    System.out.println("=== MV END ===");
-
+    //Ls ls = new Ls();
+    //System.out.println("=== LS BEGIN ===");
+    //ls.run();
+    //System.out.println("=== LS END ===");
+    //
+    //System.out.println("=== MV BEGIN ===");
+    //Mv mv = new Mv();
+    //mv.run();
+    //System.out.println("=== MV END ===");
+    //
     System.out.println("=== IfConfig Begin ===");
     IfConfig ifConfig = new IfConfig();
     ifConfig.run();
--- src/main/java/com/caresle/junix/IfConfig.java
+++ src/main/java/com/caresle/junix/IfConfig.java
@@ -1,11 +1,59 @@
 package com.caresle.junix;
 
+import java.net.*;
+import java.util.Enumeration;
+
 /**
  * IfConfig
  */
 public class IfConfig extends Command {
   @Override
   public void run() {
-    System.out.println("If Config file");    
+    try {
+      Enumeration<NetworkInterface> networks = NetworkInterface.getNetworkInterfaces();
+
+      while (networks.hasMoreElements()) {
+        NetworkInterface network = networks.nextElement(); 
+
+        if (!network.isUp() || network.isLoopback()) {
+          continue;
+        }
+
+        System.out.println("Interface:" + network.getName());
+        System.out.println("Display name:" + network.getDisplayName());
+
+        byte[] mac = network.getHardwareAddress();
+
+        if (mac != null) {
+          System.out.print("MAC:");
+          for (int i = 0; i < mac.length; i++) {
+            System.out.printf("%02X%s", mac[i], (i < mac.length - 1) ? "-" : "");
+          }
+          System.out.println();
+        }
+        
+        // flags
+        System.out.println("mtu: " + network.getMTU());
+        System.out.println("virtual: " + network.isVirtual());
+        System.out.println("Support multicast: " + network.supportsMulticast());
+
+        Enumeration<InetAddress> inets = network.getInetAddresses();
+
+        while (inets.hasMoreElements()) {
+          InetAddress inet = inets.nextElement();
+
+          System.out.println("inet: " + inet.getHostAddress());
+          if (inet instanceof Inet4Address) {
+            System.out.println("Type: IPv4");
+          }
+
+          if (inet instanceof Inet6Address) {
+            System.out.println("Type: IPv6");
+          }
+        }
+      }
+    } catch (SocketException e) {
+      System.err.println(e.getMessage());
+    }
   }
 }
