e2cb7c2df7f8a64ed61c1582d33ed79a3ea0befe
Caresle <carabesle@gmail.com>
2025-01-22 20:07:42 -0600

    feat: add get args and pass to the command mv

--- src/main/java/com/caresle/junix/App.java
+++ src/main/java/com/caresle/junix/App.java
@@ -2,20 +2,31 @@ package com.caresle.junix;
 
 public class App {
   public static void main(String[] args) {
-    // Temp main method, just testing the implementation of the commands
-    //Ls ls = new Ls();
-    //System.out.println("=== LS BEGIN ===");
-    //ls.run();
-    //System.out.println("=== LS END ===");
-    //
-    //System.out.println("=== MV BEGIN ===");
-    //Mv mv = new Mv();
-    //mv.run();
-    //System.out.println("=== MV END ===");
-    //
-    System.out.println("=== IfConfig Begin ===");
-    IfConfig ifConfig = new IfConfig();
-    ifConfig.run();
-    System.out.println("=== IfConfig END ===");
+
+    if (args.length <= 0) {
+      System.out.println("Please specify one of the commands <ls, ifconfig, mv>");
+      return;
+    }
+
+    String commandName = args[0];
+
+    if (commandName.equals("ls")) {
+      Ls ls = new Ls();
+      ls.run();
+    }
+
+    if (commandName.equals("mv")) {
+      if (args.length != 3) {
+        System.out.println("Please specify [originalPath] [destinyPath]");
+        return;
+      }
+      Mv mv = new Mv();
+      mv.run(args[1], args[2]);
+    }
+
+    if (commandName.equals("ifconfig")) {
+      IfConfig ifConfig = new IfConfig();
+      ifConfig.run();
+    }
   }
 }
--- src/main/java/com/caresle/junix/Mv.java
+++ src/main/java/com/caresle/junix/Mv.java
@@ -5,24 +5,23 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class Mv extends Command {
-    @Override
-    public void run() {
-      Path originalFile = Paths.get("./COPY_README.md");
-      Path targetDir = Paths.get("./test");
+public class Mv {
+  public void run(String originalPath, String destinyPath) {
+    Path originalFile = Paths.get(originalPath);
+    Path targetDir = Paths.get(destinyPath);
 
-      try {
-        if (!Files.exists(targetDir)) {
-          Files.createDirectory(targetDir);
-        }
-
-        Path targetFile = targetDir.resolve(originalFile.getFileName());
-        Files.move(originalFile, targetFile);
-      
-        System.out.println("File moved sucessfully to: " + targetFile);
-        
-      } catch (IOException e) {
-        System.err.println("Error moving the file: " + e.getMessage());
+    try {
+      if (!Files.exists(targetDir)) {
+        Files.createDirectory(targetDir);
       }
+
+      Path targetFile = targetDir.resolve(originalFile.getFileName());
+      Files.move(originalFile, targetFile);
+    
+      System.out.println("File moved sucessfully to: " + targetFile);
+      
+    } catch (IOException e) {
+      System.err.println("Error moving the file: " + e.getMessage());
     }
+  }
 }
